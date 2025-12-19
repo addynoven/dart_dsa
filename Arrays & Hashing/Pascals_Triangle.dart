99a2a998b17e36cebae82a66276d4fbf4876a90f
addynoven <dmcbaditya@gmail.com>
2025-12-19 16:15:11 +0530

    Add implementation of Pascal's Triangle in Pascals_Triangle.dart

---
+++ Arrays & Hashing/Pascals_Triangle.dart
@@ -0,0 +1,58 @@
+void main(List<String> args) {
+  Solution sol = Solution();
+  int numRows = 5;
+  List<List<int>> result = sol.generate(numRows);
+  List<List<int>> expected = [
+    [1],
+    [1, 1],
+    [1, 2, 1],
+    [1, 3, 3, 1],
+    [1, 4, 6, 4, 1],
+  ];
+  bool isEqual = true;
+  if (result.length != expected.length) {
+    isEqual = false;
+  } else {
+    for (int i = 0; i < result.length; i++) {
+      if (result[i].length != expected[i].length) {
+        isEqual = false;
+        break;
+      }
+      for (int j = 0; j < result[i].length; j++) {
+        if (result[i][j] != expected[i][j]) {
+          isEqual = false;
+          break;
+        }
+      }
+      if (!isEqual) {
+        break;
+      }
+    }
+  }
+  print(isEqual);
+}
+
+class Solution {
+  List<List<int>> generate(int numRows) {
+    if (numRows == 0) {
+      return <List<int>>[];
+    }
+    List<List<int>> triangle = [
+      [1],
+    ];
+    if (numRows == 1) {
+      return triangle;
+    }
+    triangle.add([1, 1]);
+    for (var i = 2; i < numRows; i++) {
+      List<int> last_row = triangle[i - 1];
+      List<int> curr_row = List<int>.filled(i + 1, 1, growable: false);
+      for (var j = 1; j < curr_row.length - 1; j++) {
+        curr_row[j] = last_row[j - 1] + last_row[j];
+      }
+      triangle.add(curr_row);
+    }
+    print(triangle);
+    return triangle;
+  }
+}
